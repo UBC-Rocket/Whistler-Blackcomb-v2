c65fb8b4dceca9a88e59820819e21714243116ec
Alice-Xiong <alicexxx0516@gmail.com>
2020-09-20 14:55:20 -0700

    Added RTOS libraries and blink task

--- source/Whistler-Blackcomb.c
+++ source/Whistler-Blackcomb.c
@@ -18,29 +18,21 @@
 // Not sure why this wasn't required in blink sketch
 #include "fsl_gpio.h"
 
+/* FreeRTOS kernel includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+
 
 // Definitions
 #define BOARD_LED_GPIO     BOARD_LED_BUILTIN_GPIO
 #define BOARD_LED_GPIO_PIN BOARD_INITPINS_LED_BUILTIN_PIN
 
-
-// Variables
-volatile uint32_t g_systickCounter;
-
-// Functions
-void SysTick_Handler(void)
-{
-    if (g_systickCounter != 0U)
-    {
-        g_systickCounter--;
-    }
-}
-
-void SysTick_DelayTicks(uint32_t n)
-{
-    g_systickCounter = n;
-    while (g_systickCounter != 0U)
-    {
+/* simple task */
+static void MyTask(void *pv) {
+    while (1){
+   	 GPIO_PortToggle(BOARD_LED_GPIO, 1u << BOARD_LED_GPIO_PIN);
+   	 // Very important: Don't use normal delays in RTOS tasks, things will break
+   	 vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
 
@@ -53,19 +45,23 @@ int main(void) {
     BOARD_InitBootClocks();
     BOARD_InitBootPeripherals();
 
-    /* Set systick reload value to generate 1ms interrupt */
-	if (SysTick_Config(SystemCoreClock / 1000U))
-	{
-		while (1)
-		{
-		}
-	}
+    // Copy the following to create a new task
+    if (xTaskCreate(  /* create task */
+        MyTask,  /* pointer to the task */
+        "App", /* task name for kernel awareness debugging */
+        200/sizeof(StackType_t), /* task stack size */
+        (void*)NULL, /* optional task startup argument */
+        tskIDLE_PRIORITY+2,  /* initial priority */
+        (TaskHandle_t*)NULL /* optional task handle to create */
+      ) != pdPASS) {
+       for(;;){} /* error! probably out of memory */
+    }
+
+     vTaskStartScheduler();
 
 	while (1)
 	{
-		/* Delay 1000 ms */
-		SysTick_DelayTicks(1000U);
-		GPIO_PortToggle(BOARD_LED_GPIO, 1u << BOARD_LED_GPIO_PIN);
+
 	}
 
 //    printf("Hello World\n");
