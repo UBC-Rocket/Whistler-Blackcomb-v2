1011dc18f3569185280f2a6953eca30bcc246903
Esouder <37464714+Esouder@users.noreply.github.com>
2020-10-25 15:13:51 -0700

    Update basicTest.c
    now the test should fail

--- test/basicTest.c
+++ test/basicTest.c
@@ -3,11 +3,15 @@
 void baseTest(void){
     TEST_ASSERT_EQUAL_HEX8(2,2);
 }
+void baseTest2(void){
+    TEST_ASSERT_EQUAL_HEX8(1,2);
+}
 
 void setUp(void){}
 void tearDown(void){}
 int main (void){
     UNITY_BEGIN();
     RUN_TEST(baseTest);
+    RUN_TEST(baseTest2);
     return UNITY_END();
 }
