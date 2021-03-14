d92c5573dbb7a247bfc0d2886912f52790734b54
Esouder <37464714+Esouder@users.noreply.github.com>
2021-03-14 01:21:51 -0800

    Finished all quaternion and linear algebra tests; float eval fix
    - Added linear algebra tests
    - Added quaternion tests
    - updated way test functions are called
    - room to include more/better test cases.
    - fixed where Unity was evaluating some unequal floats as being equal

--- test/test_prediction.c
+++ test/test_prediction.c
@@ -5,9 +5,16 @@ void compareSquareMatrix(double testArray[MATRIX_SIZE][MATRIX_SIZE],
     double expectedArray[MATRIX_SIZE][MATRIX_SIZE]){
      for(int i=0;i<MATRIX_SIZE;i++){
         for(int ii=0;ii<MATRIX_SIZE;ii++){
-            TEST_ASSERT_EQUAL(expectedArray[i][ii],testArray[i][ii]);
+            TEST_ASSERT_EQUAL_FLOAT(expectedArray[i][ii],testArray[i][ii]);
         }
     }
+}
+
+void compareQ(quaternion testQ, quaternion expectedQ){
+    TEST_ASSERT_EQUAL_FLOAT(expectedQ.re, testQ.re);
+    TEST_ASSERT_EQUAL_FLOAT(expectedQ.i, testQ.i);
+    TEST_ASSERT_EQUAL_FLOAT(expectedQ.j, testQ.j);
+    TEST_ASSERT_EQUAL_FLOAT(expectedQ.k, testQ.k);
 }
 
 void baseTest(void){
@@ -110,46 +117,262 @@ void addSubtractVecTest(void){
     TEST_ASSERT_EQUAL_HEX8_ARRAY(expectedSubVec,resultVec,MATRIX_SIZE);
 }
 
-void adjointTest(void){
-    double testMat[][MATRIX_SIZE] = {{1,2},{3,4}};
-    double resultMat[MATRIX_SIZE][MATRIX_SIZE];
-    double expectedResultMat[][MATRIX_SIZE] = {{4,-2},{-3,1}};
-
-    adjoint(testMat,resultMat);
-    
-    compareSquareMatrix(resultMat,expectedResultMat);
-}
-
 void inverseTest(void){
-    double testMat[][MATRIX_SIZE] = {{1,2},{3,4}};
+    double testMat[][MATRIX_SIZE] = {{1.0,2.0},{3.0,4.0}};
     double resultMat[MATRIX_SIZE][MATRIX_SIZE];
-    double expectedResultMat[][MATRIX_SIZE] = {{-2,1},{3/2,-1/2}};
+    double expectedResultMat[][MATRIX_SIZE] = {{-2.0,1.0},{3.0/2,-1.0/2}};
 
     inverse(testMat,resultMat);
     
     compareSquareMatrix(resultMat,expectedResultMat);
 }
-/**
- * Qs:
- * WTF is cofactor
- * What's n in determinant
- * what's adjoint
 
-*/
+void qMultTest(void){
+    quaternion testQ0;
+    testQ0.re=0;
+    testQ0.i=0;
+    testQ0.j=0;
+    testQ0.k=0;
 
+    quaternion testQ1;
+    testQ1.re=1;
+    testQ1.i=2;
+    testQ1.j=3;
+    testQ1.k=4;
+
+    quaternion testQ2;
+    testQ2.re=-5;
+    testQ2.i=-6;
+    testQ2.j=-7;
+    testQ2.k=-8;
+
+    quaternion resultQ1m0;
+    quaternion resultQ1m2;
+    quaternion resultQ2m1;
+    quaternion resultQ1m1;
+    quaternion resultQ2m2;
+
+    quaternion expectedResultQ1m0; //Q1 multiply Q0
+    expectedResultQ1m0.re=0; 
+    expectedResultQ1m0.i=0;
+    expectedResultQ1m0.j=0;
+    expectedResultQ1m0.k=0;
+
+    quaternion expectedResultQ1m2;
+    expectedResultQ1m2.re= 60;
+    expectedResultQ1m2.i=-12;
+    expectedResultQ1m2.j=-30;
+    expectedResultQ1m2.k=-24;
+
+    quaternion expectedResultQ2m1;
+    expectedResultQ2m1.re= 60;
+    expectedResultQ2m1.i=-20;
+    expectedResultQ2m1.j=-14;
+    expectedResultQ2m1.k=-32;
+
+    quaternion expectedResultQ1m1;
+    expectedResultQ1m1.re= -28;
+    expectedResultQ1m1.i=4;
+    expectedResultQ1m1.j=6;
+    expectedResultQ1m1.k=8;
+
+    quaternion expectedResultQ2m2;
+    expectedResultQ2m2.re= -124;
+    expectedResultQ2m2.i=60;
+    expectedResultQ2m2.j=70;
+    expectedResultQ2m2.k=80;
+
+    resultQ1m0 = qMult(testQ1,testQ0);
+    resultQ1m2=qMult(testQ1,testQ2);
+    resultQ2m1=qMult(testQ2,testQ1);
+    resultQ1m1 = qMult(testQ1,testQ1);
+    resultQ2m2 = qMult(testQ2,testQ2);
+
+    compareQ(resultQ1m0,expectedResultQ1m0);
+    compareQ(resultQ1m2,expectedResultQ1m2);
+    compareQ(resultQ2m1,expectedResultQ2m1);
+    compareQ(resultQ1m1,expectedResultQ1m1);
+    compareQ(resultQ2m2,expectedResultQ2m2);
+}
+
+void qSumTest(void){
+    quaternion testQ0;
+    testQ0.re=0;
+    testQ0.i=0;
+    testQ0.j=0;
+    testQ0.k=0;
+
+    quaternion testQ1;
+    testQ1.re=1;
+    testQ1.i=2;
+    testQ1.j=3;
+    testQ1.k=4;
+
+    quaternion testQ2;
+    testQ2.re=-5;
+    testQ2.i=-6;
+    testQ2.j=-7;
+    testQ2.k=-8;
+
+    quaternion resultQ1s0;
+    quaternion resultQ1s2;
+    quaternion resultQ2s1;
+    quaternion resultQ1s1;
+    quaternion resultQ2s2;
+
+    quaternion expectedResultQ1s0; //Q1 sum Q0
+    expectedResultQ1s0.re=1; 
+    expectedResultQ1s0.i=2;
+    expectedResultQ1s0.j=3;
+    expectedResultQ1s0.k=4;
+
+    quaternion expectedResultQ1s2;
+    expectedResultQ1s2.re= -4;
+    expectedResultQ1s2.i=-4;
+    expectedResultQ1s2.j=-4;
+    expectedResultQ1s2.k=-4;
+
+    quaternion expectedResultQ2s1;
+    expectedResultQ2s1.re= -4;
+    expectedResultQ2s1.i=-4;
+    expectedResultQ2s1.j=-4;
+    expectedResultQ2s1.k=-4;
+
+    quaternion expectedResultQ1s1;
+    expectedResultQ1s1.re= 2;
+    expectedResultQ1s1.i=4;
+    expectedResultQ1s1.j=6;
+    expectedResultQ1s1.k=8;
+
+    quaternion expectedResultQ2s2;
+    expectedResultQ2s2.re= -10;
+    expectedResultQ2s2.i=-12;
+    expectedResultQ2s2.j=-14;
+    expectedResultQ2s2.k=-16;
+
+    resultQ1s0 = qSum(testQ1,testQ0);
+    resultQ1s2=qSum(testQ1,testQ2);
+    resultQ2s1=qSum(testQ2,testQ1);
+    resultQ1s1 = qSum(testQ1,testQ1);
+    resultQ2s2 = qSum(testQ2,testQ2);
+
+    compareQ(resultQ1s0,expectedResultQ1s0);
+    compareQ(resultQ1s2,expectedResultQ1s2);
+    compareQ(resultQ2s1,expectedResultQ2s1);
+    compareQ(resultQ1s1,expectedResultQ1s1);
+    compareQ(resultQ2s2,expectedResultQ2s2);
+}
+
+void qNormTest(void){
+
+    quaternion testQ1;
+    testQ1.re=1;
+    testQ1.i=2;
+    testQ1.j=3;
+    testQ1.k=4;
+
+    quaternion testQ2;
+    testQ2.re=-5;
+    testQ2.i=-6;
+    testQ2.j=-7;
+    testQ2.k=-8;
+
+    quaternion resultQ1 = qNorm(testQ1);
+
+    quaternion resultQ2 = qNorm(testQ2);
+
+    quaternion expectedResultQ1;
+    expectedResultQ1.re=1.0/sqrt(30);
+    expectedResultQ1.i=2.0/sqrt(30);
+    expectedResultQ1.j=3.0/sqrt(30);
+    expectedResultQ1.k=4.0/sqrt(30);
+
+    quaternion expectedResultQ2;
+    expectedResultQ2.re= -5.0/sqrt(174);
+    expectedResultQ2.i= -6.0/sqrt(174);
+    expectedResultQ2.j= -7.0/sqrt(174);
+    expectedResultQ2.k= -8.0/sqrt(174);
+
+    compareQ(resultQ1,expectedResultQ1);
+    compareQ(resultQ2,expectedResultQ2);
+}
+
+void qConjugateTest(void){
+
+    quaternion testQ0;
+    testQ0.re=0;
+    testQ0.i=0;
+    testQ0.j=0;
+    testQ0.k=0;
+
+    quaternion testQ1;
+    testQ1.re=1;
+    testQ1.i=2;
+    testQ1.j=3;
+    testQ1.k=4;
+
+    quaternion testQ2;
+    testQ2.re=-5;
+    testQ2.i=-6;
+    testQ2.j=-7;
+    testQ2.k=-8;
+
+    quaternion resultQ0 = qConjugate(testQ0);
+
+    quaternion resultQ1 = qConjugate(testQ1);
+
+    quaternion resultQ2 = qConjugate(testQ2);
+
+    quaternion expectedResultQ0;
+    expectedResultQ0.re=0;
+    expectedResultQ0.i=0;
+    expectedResultQ0.j=0;
+    expectedResultQ0.k=0;
+    
+    quaternion expectedResultQ1;
+    expectedResultQ1.re=1;
+    expectedResultQ1.i=-2;
+    expectedResultQ1.j=-3;
+    expectedResultQ1.k=-4;
+
+    quaternion expectedResultQ2;
+    expectedResultQ2.re= -5;
+    expectedResultQ2.i= 6;
+    expectedResultQ2.j= 7;
+    expectedResultQ2.k= 8;
+
+    compareQ(resultQ0,expectedResultQ0);
+    compareQ(resultQ1,expectedResultQ1);
+    compareQ(resultQ2,expectedResultQ2);
+}
+
+
+
+
+
+/*helper array of all the tests, for simplifying main() and just to try it out*/
+void (*predictionTests[])(void)={
+    baseTest,
+    matMultTest,
+    transposeTest,
+    matVecMultTest,
+    scalMultTest,
+    addSubtractMatTest,
+    addSubtractVecTest,
+    inverseTest,
+    qMultTest,
+    qSumTest,
+    qNormTest,
+    qConjugateTest,
+};
 
 void setUp(void){}
 void tearDown(void){}
 int main (void){
     UNITY_BEGIN();
-    RUN_TEST(baseTest);
-    RUN_TEST(matMultTest);
-    RUN_TEST(transposeTest);
-    RUN_TEST(matVecMultTest);
-    RUN_TEST(scalMultTest);
-    RUN_TEST(addSubtractMatTest);
-    RUN_TEST(addSubtractVecTest);
-    RUN_TEST(adjointTest);
-    RUN_TEST(inverseTest);
+    int numTests = (sizeof(predictionTests) / sizeof(*(predictionTests)));
+    for(int testNum=0;testNum<numTests;testNum++){
+        RUN_TEST(predictionTests[testNum]);
+    }
     return UNITY_END();
 }
